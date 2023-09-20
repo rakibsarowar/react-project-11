7f06e97e59f19d40c3ede26a9cb6339a24f89af9
rakibsarowar <rakibsarowar@gmail.com>
2023-09-20 22:16:29 +0600

    working on Nav

--- 20profile/src/components/Nav.jsx
+++ 20profile/src/components/Nav.jsx
@@ -4,16 +4,18 @@ import React from 'react';
 const Nav = () => {
     return (
         <div className='sm:pt-5 sm:pb-5 w-full bg-gray-900 border-gray-800 border-b'>
-            <div className='container mx-auto'>
+            <div className='container px-5 mx-auto'>
                 <div className='grid grid-cols-12 gap-4'>
-                    <div className='col-span-12 xl:col-span-8 xl:col-start-3'>
+                    <div className='col-span-12 px-0 sm:col-start-1 sm:col-span-12 sm:px-0 lg:col-start-1 lg:col-span-12 lg:px-0 xl:col-span-8 xl:col-start-3 2xl:col-span-8 2xl:col-start-3'>
                         <nav className='items-center justify-between lg:justify-start'>
-                            <nav>
-                                <div className=''>
+                            <nav className='items-center justify-between'>
+                                <div className='md:block md:ml-0 md:pr-4 md:space-x-8'>
                                     
                                     <a aria-current="page" direction="up" className="font-bold text-gray-300 hover:text-white mr-10 md:mr-0" href="/#">Hello !</a>
 
-                                    <a direction="up" className='font-medium text-gray-300 hover:text-white mr-10 md:mr-0 '> Projects</a>
+                                    <a direction="up" className='font-medium text-gray-300 hover:text-white mr-10 md:mr-0' href='#projects'> Projects</a>
+
+                                    <a direction="up" className='font-medium text-gray-300 hover:text-white mr-10 md:mr-0' href='skills'> Skills </a>
 
                                     
                                 </div>
